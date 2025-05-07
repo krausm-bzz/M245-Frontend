16db240b04e59d4a88da1d0b8df7b9f77c64e9e7
BarmettlerL-bzz <BarmettlerL@bzz.ch>
2025-05-07 13:15:56 +0200

    added the forgotten routes to router.jsx and the interpretation of App from chatgpt in current state (App.jsx)

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,9 @@
 import Impressum from './pages/Impressum';
 import Datenschutz from './pages/Datenschutz';
+import AGB from './pages/AGB';
+import Widerruf from './pages/Widerruf';
 
 <Route path="/impressum" element={<Impressum />} />
 <Route path="/datenschutz" element={<Datenschutz />} />
+<Route path="/agb" element={<AGB />} />
+<Route path="/widerruf" element={<Widerruf />} />
