e1fb74797e0a0bdd45fa5dadc881c0137ea83a8b
BarmettlerL-bzz <BarmettlerL@bzz.ch>
2025-05-14 10:01:50 +0200

    branchswap (small change in login)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-//import { loginUser } from '../services/api';
+import { loginUser } from '../services/api';
 
 function Login() {
     const [email, setEmail] = useState('');
